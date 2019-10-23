53d76575ccdd421da29fa1aaa32f4ae7caa38040
Casper van Battum <cvanbattum@gmail.com>
2019-10-23 16:22:05 +0200

    Initial commit

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour {
+	[SerializeField] private float speedY;
+	[SerializeField] private float speedX;
+	
+	// Mouselook script
+	private void Update() {
+		float rotY = Input.GetAxis("Mouse X");
+		float rotX = Input.GetAxis("Mouse Y");
+
+		transform.eulerAngles += new Vector3(rotX * speedX, rotY * speedY);
+	}
+}

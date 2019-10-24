a310693619f8511e54fc1e4d185689fb51b9a2dd
noy4 <naoyan418@gmail.com>
2019-10-24 14:03:27 +0200

    Launching

---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject sattelitePrefab;
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject sattelite = Instantiate(sattelitePrefab, transform.position, Quaternion.identity);
+            sattelite.transform.LookAt(target.transform.position);
+
+            Vector3 vector = target.transform.position - transform.position;
+            vector.Normalize();
+            sattelite.GetComponent<Rigidbody>().AddForce(vector * speed);
+        }
+    }
+}

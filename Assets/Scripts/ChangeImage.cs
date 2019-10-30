96d1f621a55be64511a2fd6c32ef296260667665
noy4 <naoyan418@gmail.com>
2019-10-30 02:19:57 +0100

    Added info images

---
+++ Assets/Scripts/ChangeImage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+    public Image image;
+    private Sprite sprite;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            sprite = Resources.Load<Sprite>("Earth_info2");
+            image = this.GetComponent<Image>();
+            image.sprite = sprite;
+
+        }
+    }
+}
